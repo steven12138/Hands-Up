7b7d510a6ecf0db64498840ba891d9035c090494
steven12138 <m18611209860@163.com>
2019-05-12 22:18:23 +0800

    do the helloworld of pixi fix bug
    draw a black block
    fix the bug that can't not include other js files

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var path =  require('path')
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/MainActivity.html');
