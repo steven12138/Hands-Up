d99c91ad906e640fdf42580573212a1869c511d5
steven12138 <m18611209860@163.com>
2019-05-13 20:50:11 +0800

    load more texture
    update renderer.js to let the renderer load more texture

--- renderer.js
+++ renderer.js
@@ -14,6 +14,9 @@ let Map;
 
 PIXI.loader
 	.add("police-normal","Image/MainChara-police-normal.png")
+	.add("police-lean","Image/MainChara-police-lean.png")
+	.add("thief-normal","Image/MainChara-thief-normal")
+	.add("theif-lean","Image/MainChara-thief-lean")
 	.add("Map","Image/testmap.png")
 	.load(setup);
 
